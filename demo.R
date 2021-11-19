7b6e7c1cc36abf76b72dcf031f506a945daa2472
maidana <melisamaidana@Melisas-MacBook-Pro.local>
2021-11-19 11:02:31 -0800

    Ex. 5

--- demo.R
+++ demo.R
@@ -2,15 +2,19 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg0> [<arg1>] --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>]
 Options:
-<arg0>             Takes any value (this is a required positional argument)
-<arg1>             Takes any value (this is an optional positional argument)
+<arg1>            Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[<arg4>]          Takes any value (this is an optional positional argument)
+
 " -> doc
 
 library(docopt)
+
 opt <- docopt(doc)
+
 print(opt)
 print(typeof(opt))
+print(opt$arg4)
